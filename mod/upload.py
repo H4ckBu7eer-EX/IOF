d1225be00bcdb9e073d9b608827485cdc0f3a138
H4CKBUEER_EX <96736106+H4ckBu7eer-EX@users.noreply.github.com>
2023-05-19 00:39:54 +0800

    Update upload.py

--- mod/upload.py
+++ mod/upload.py
@@ -2,7 +2,7 @@ def upload(version):
     import requests
     print("当前版本为V"+str(version))
     print("检查更新中...")
-    url = "https://h4ckbu7eer-ex.github.io/H4ckBu7eer-EX/upload.html"
+    url = "https://h4ckbu7eer-ex.github.io/H4ckBu7eer-EX/iofupload.html"
     response = requests.get(url)
     if response.status_code == 200:
         print("最新版本为V"+str(int(response.text)))
